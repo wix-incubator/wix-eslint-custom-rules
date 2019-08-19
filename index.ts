45165cf66a2a8b24884275b02b1e923ec9e2e60d
eran shabi <eranshabi@gmail.com>
2019-08-19 18:07:59 +0300

    publish rules

---
+++ index.ts
@@ -0,0 +1,7 @@
+import noWallabyFileOnly from './rules/no-wallaby-file-only';
+import wixstoresFileNames from './rules/wixstores-file-names';
+
+export const rules = {
+    'no-wallaby-file-only': noWallabyFileOnly,
+    'wixstores-file-names': wixstoresFileNames
+};
